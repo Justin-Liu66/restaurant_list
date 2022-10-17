891511d16fd7095720bcfdfe813aa9365ab69cbd
Justin <s101407028@gmail.com>
2022-10-17 09:18:06 +0800

    debug: sortSelector.js

--- utility/sortSelector.js
+++ utility/sortSelector.js
@@ -1,23 +1,22 @@
 function sortSelector(sort) {
-  /* 方法一
+  /*方法一
   switch (sort) {
     case 'name_asc':
       return { name: 'asc' }
     case 'name_desc':
       return { name: 'desc' }
-    case 'category':
+    case 'category_asc':
       return { category: 'asc' }
-    case 'location':
+    case 'location_asc':
       return { location: 'asc' }
   } */
+
   //方法二
+  if(!sort) { //若點擊首頁(無sort排序)，則不必往下執行，否則會出現錯誤
+    return
+  }
   const [property, sortBy] = sort.split('_')
   return { [property]: sortBy }
 }
+
 module.exports = sortSelector
-
-
-//sort = 'name_asc'
-//const [property, sortBy] = sort.split('_')
-//['name', 'asc']
-//property = 'name' sortBy='asc'
