e42c70d8b742dbac57e032acf84dbbbe4cdabcbe
Justin <s101407028@gmail.com>
2022-10-31 20:37:05 +0800

    feat: add user model

---
+++ models/users.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose') //載入mongoose
+const Schema = mongoose.Schema //mongoose提供的模組
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+module.exports = mongoose.model('User', userSchema)
