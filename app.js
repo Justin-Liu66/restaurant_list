a326cd06e286e16d4b2e639b2c7f4f288c1762ba
Justin <s101407028@gmail.com>
2022-11-01 10:48:09 +0800

    feat: add isAuthenticated to display different view templates

--- app.js
+++ app.js
@@ -40,6 +40,14 @@ app.use(methodOverride('_method'))
 // 呼叫 Passport 函式並傳入 app
 usePassport(app)
 
+//將req驗證完的登入狀態交接給res
+app.use((req, res, next) => {
+  //console.log(req.user)
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  next()
+})
+
 //設定每一筆請求都要導入路由器
 app.use(routes)
 
